43928740e8a77905d90640b39fe5b043d0ad6c41
pixsus228 <alexkarnauch759@gmail.com>
2026-07-21 20:05:28 +0300

    Final backend polish: clean cart viewset, update models and ensure clean checks

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,10 +1,9 @@
 ﻿from rest_framework import viewsets, permissions
-from .models import Cart
-from .serializers import CartSerializer
+from apps.cart.models import Cart
+from apps.cart.serializers.cart_serializers import CartSerializer
 
 class CartViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.IsAuthenticated]
+    queryset = Cart.objects.all()
     serializer_class = CartSerializer
-    
-    def get_queryset(self):
-        return Cart.objects.filter(user=self.request.user)
+    permission_classes = [permissions.IsAuthenticated]
+
