abae1236929af5293226025d8a09c66bb341a06d
pixsus228 <alexkarnauch759@gmail.com>
2026-08-11 19:57:13 +0300

    fix: оновлено конфігурацію api_urls для проходження тестів

--- apps/shop/api_urls.py
+++ apps/shop/api_urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from apps.shop.views import ProductViewSet
-from apps.shop.models import Size, Product
+from apps.shop.views import ProductViewSet, SizeViewSet
 from apps.shop.serializers.product import ProductSerializer
+from apps.shop.models import Product, Size
 from rest_framework import viewsets
 
-class SizeSerializer(viewsets.serializers.ModelSerializer):
+app_name = 'shop'
+
+class SizeSerializer(viewsets.ModelSerializer):
     class Meta:
         model = Size
         fields = '__all__'
@@ -14,9 +16,13 @@ class SizeViewSet(viewsets.ModelViewSet):
     queryset = Size.objects.all()
     serializer_class = SizeSerializer
 
-# Явно задаємо basename, щоб DRF створював правильні реверси 'product-list' та 'product-detail'
+class ProductAPISet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "slug"
+
 router = DefaultRouter()
-router.register(r'products', ProductViewSet, basename='product')
+router.register(r'products', ProductAPISet, basename='product')
 router.register(r'sizes', SizeViewSet, basename='size')
 
 urlpatterns = [
