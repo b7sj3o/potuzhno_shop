9ba5b144ccae7a7691290e3cd7a547f84e55b25f
pixsus228 <alexkarnauch759@gmail.com>
2026-08-11 19:52:16 +0300

    fix: оновлено роутер DRF для коректного реверсу імен

--- apps/shop/api_urls.py
+++ apps/shop/api_urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.shop.views import ProductViewSet
-from apps.shop.models import Size
-from rest_framework import viewsets
+from apps.shop.models import Size, Product
 from apps.shop.serializers.product import ProductSerializer
-from apps.shop.models import Product
+from rest_framework import viewsets
 
 class SizeSerializer(viewsets.serializers.ModelSerializer):
     class Meta:
@@ -15,13 +14,9 @@ class SizeViewSet(viewsets.ModelViewSet):
     queryset = Size.objects.all()
     serializer_class = SizeSerializer
 
-class ProductAPISet(viewsets.ModelViewSet):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_field = "slug"
-
+# Явно задаємо basename, щоб DRF створював правильні реверси 'product-list' та 'product-detail'
 router = DefaultRouter()
-router.register(r'products', ProductAPISet, basename='product-list')
+router.register(r'products', ProductViewSet, basename='product')
 router.register(r'sizes', SizeViewSet, basename='size')
 
 urlpatterns = [
