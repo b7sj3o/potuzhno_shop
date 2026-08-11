f9e693107178b3a0874a308b152bd8211d8ba73c
pixsus228 <alexkarnauch759@gmail.com>
2026-08-11 19:46:39 +0300

    fix: виправлено basename для product-list у DRF роутері

--- apps/shop/api_urls.py
+++ apps/shop/api_urls.py
@@ -1,31 +1,28 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework import serializers
 from apps.shop.views import ProductViewSet
 from apps.shop.models import Size
-
-class SizeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Size
-        fields = '__all__'
-
-class SizeViewSet(DefaultRouter.ViewSet if hasattr(DefaultRouter, 'ViewSet') else __import__('rest_framework').viewsets.ModelViewSet):
-    queryset = Size.objects.all()
-    serializer_class = SizeSerializer
-
 from rest_framework import viewsets
 from apps.shop.serializers.product import ProductSerializer
 from apps.shop.models import Product
 
-# Оновлений ProductViewSet з lookup_field = "slug"
-class FixedProductViewSet(viewsets.ModelViewSet):
+class SizeSerializer(viewsets.serializers.ModelSerializer):
+    class Meta:
+        model = Size
+        fields = '__all__'
+
+class SizeViewSet(viewsets.ModelViewSet):
+    queryset = Size.objects.all()
+    serializer_class = SizeSerializer
+
+class ProductAPISet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "slug"
 
 router = DefaultRouter()
-router.register(r'products', FixedProductViewSet, basename='product-api')
-router.register(r'sizes', SizeViewSet, basename='size-api')
+router.register(r'products', ProductAPISet, basename='product-list')
+router.register(r'sizes', SizeViewSet, basename='size')
 
 urlpatterns = [
     path('', include(router.urls)),
