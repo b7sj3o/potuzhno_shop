2492f9e210522586cd136bf54d861cac7c31f433
Vitaliy <vitalikfedytnyk@gmail.com>
2026-08-18 23:22:41 +0300

    module 3; lvl 19

--- potuzhno_shop/apps/api/permissions.py
+++ potuzhno_shop/apps/api/permissions.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsStaffOrReadOnly(BasePermission):
     message = "Змінювати каталог / Фільтрувати відгуки може лише персонал магазину"
 
-    def has_permissioin(self, request, view):
+    def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
 
--- potuzhno_shop/apps/api/views.py
+++ potuzhno_shop/apps/api/views.py
@@ -1,6 +1,8 @@
 from rest_framework import status, viewsets
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -55,18 +57,67 @@ class ProductViewSet(viewsets.ModelViewSet):
         response = ProductReadSerializer(product, context=self.get_serializer_context())
         return Response(response.data, status=status.HTTP_201_CREATED)
 
+    @action(detail=False, methods=["get"])
+    def featured(self, request):
+        products = self.filter_queryset(self.get_queryset()).filter(is_featured=True)
+
+        page = self.paginate_queryset(products)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
+
+    # products/<id>/reviews
+    @action(detail=True, methods=["get"])
+    def reviews(self, request, pk=None):
+        # reviews = Review.objects.filter(product_id=pk)
+        product = self.get_object()
+        reviews = (
+            product.reviews
+            .select_related("user")
+            .order_by("-created_at")
+        )
+
+        page = self.paginate_queryset(reviews)
+        if page is not None:
+            serializer = ReviewReadSerializer(page, many=True, context=self.get_serializer_context())
+            return self.get_paginated_response(serializer.data)
+
+        serializer = ReviewReadSerializer(reviews, many=True, context=self.get_serializer_context())
+        return Response(serializer.data)
+
+    @action(
+        detail=True,
+        methods=["post"],
+        permission_classes=[IsAuthenticated]
+    )
+    def favourite(self, request, pk=None):
+        product = self.get_object()
+
+        if request.user.profile.favourites.filter(id=pk).exists():
+            return Response({"detail": "Product already added to your favourites"}, status=status.HTTP_200_OK)
+
+        request.user.profile.favourites.add(product)
+        return Response({"detail": "Product was added to your favourites"}, status=status.HTTP_201_CREATED)
+
+    @favourite.mapping.delete
+    def remove_favourite(self, request, pk=None):
+        product = self.get_object()
+        request.user.profile.favourites.remove(product)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     permission_classes = (IsStaffOrReadOnly,)
     queryset = Category.objects.all()
 
-
 class BrandViewSet(viewsets.ModelViewSet):
     serializer_class = BrandSerializer
     permission_classes = (IsStaffOrReadOnly,)
     queryset = Brand.objects.all()
-
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.select_related("user", "product")
@@ -78,18 +129,24 @@ class ReviewViewSet(viewsets.ModelViewSet):
         return ReviewWriteSerializer
 
 
-# class ProductListCreateApiView(APIView):
-#     def get(self, request):
-#         products = Product.objects.active()
-#         serializer = ProductSerializer(products, many=True)
-#         return Response(serializer.data)
-#
-#     def post(self, request):
-#         serializer = ProductSerializer(data=request.data)
-#
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @action(
+        detail=False,
+        methods=["get"],
+        permission_classes=[IsAuthenticated]\
+    )
+    def mine(self, request):
+        reviews = (
+            self.get_queryset()
+            .filter(user=request.user)
+            .order_by("-created_at")
+        )
+
+        page = self.paginate_queryset(reviews)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(reviews, many=True)
+        return Response(serializer.data)
+
 
