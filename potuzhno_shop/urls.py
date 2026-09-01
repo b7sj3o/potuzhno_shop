4df68b81663f487fcb61001611b48b9dc6239617
pixsus228 <alexkarnauch759@gmail.com>
2026-09-01 19:36:21 +0300

    feat: integrate drf-spectacular for swagger api documentation

--- potuzhno_shop/urls.py
+++ potuzhno_shop/urls.py
@@ -1,8 +1,14 @@
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
     # JWT Tokens
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
